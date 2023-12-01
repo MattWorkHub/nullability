bf720650b3eb174d8bdcebb62093906e8f61aa40
MattWorkHub <150373956+MattWorkHub@users.noreply.github.com>
2023-12-01 15:36:41 +0100

    test

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        nullTest(null,null);
+    }
+    public static void nullTest(Integer numerator, Integer denominator) {
+        try {
+            if(numerator == null) {
+                throw new Exception("Numerator is null");
+            } else {
+                System.out.println("Numerator is " + numerator + " and is not null");
+            }
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+        try {
+            if(denominator == null) {
+                throw new Exception("Denominator is null");
+            } else {
+                System.out.println("Denominator is " + denominator + " and is not null");
+            }
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
